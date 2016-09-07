7386faa3f75224244dae22b30faa2275157fa35d
Ivar van de Guchte <ivaryese123@gmail.com>
2016-09-07 10:09:24 +0200

    Test

---
+++ DBConnection.js
@@ -0,0 +1,17 @@
+var connection = new ActiveXObject("ADODB.Connection") ;
+
+var connectionstring="Data Source="127.0.0.1";Initial Catalog=<catalog>;User ID="root";Password="";Provider=SQLOLEDB";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+
+rs.close;
+connection.close;
